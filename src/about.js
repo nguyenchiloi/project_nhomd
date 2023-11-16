c4535f626c628cac2c37f9027a3a8380c3c7d34b
quangphuc903 <21211tt4340@mail.tdc.edu.vn>
2023-11-16 21:33:00 +0700

    about

--- src/about.js
+++ src/about.js
@@ -5,20 +5,61 @@ const About = () => {
         <>
             <h1>ngon bo re</h1>
             <Row justify="center" >
-                <Col span={12} offset={11}>
-                    <Image
+                <Col span={12}>
+                    {/* <Image 
+                        width={100}
+                        src="./img/b1.png"
+                    /> */}
+                    <div style={{ textAlign: "center" }}>
+                        <div>
+                            Nguyễn Chí Lợi <br></br>
+                            Nhóm trưởng
+                        </div>
+                    </div>
+                </Col>
+
+                <Col span={12}>
+                    {/* <Image
                         width={200}
                         src="./img/b1.png"
-                    />
-                    <div style={{textAlign: "center"}}>Nguyen Chi Loi</div>
+                    /> */}
+                    <div style={{ textAlign: "center" }}>
+                        <div>
+                            Võ Xuân Huy <br></br>
+                            Nhóm phó
+                        </div>
+                    </div>
                 </Col>
-                <Col span={12}>huy</Col>
+
 
             </Row>
             <Row justify="center">
-                <Col span={8}>phuc</Col>
-                <Col span={8}>duy</Col>
-                <Col span={8}>thanh</Col>
+                <Col span={8}>
+                    <div style={{ textAlign: "center" }}>
+                        <div>
+                            Nguyễn Quang Phục <br></br>
+                            Thành viên
+                        </div>
+                    </div>
+                </Col>
+
+                <Col span={8}>
+                <div style={{ textAlign: "center" }}>
+                        <div>
+                            Lê Thanh Duy <br></br>
+                            Thành viên
+                        </div>
+                    </div>
+                </Col>
+
+                <Col span={8}>
+                <div style={{ textAlign: "center" }}>
+                        <div>
+                            Lê Đức Thành <br></br>
+                            Thành viên
+                        </div>
+                    </div>
+                </Col>
             </Row>
         </>
     )
