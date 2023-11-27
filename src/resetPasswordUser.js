5bcf059c81f2f82cf36e4c047b100941bbeceb61
quangphuc903 <21211tt4340@mail.tdc.edu.vn>
2023-11-27 20:54:12 +0700

    resetpassworduser

--- src/resetPasswordUser.js
+++ src/resetPasswordUser.js
@@ -1,11 +1,12 @@
 import { Button, Col, Form, Input, Row, message } from 'antd'
+import './resetPasswordUser.css'
 import TextArea from 'antd/es/input/TextArea'
 
 const ResetPasswordUser = () => {
    return (
       <>
          <div className='change-password'>
-            <h1 style={{textAlign: 'center'}}>Thay đổi mật khẩu người dùng</h1>
+            <h1>THAY ĐỔI MẬT KHẨU NGƯỜI DÙNG</h1>
             <div className='form'>
                <Form layout="vertical">
                   <Row className="d-flex" justify="start" gutter={[0, 15]}>
@@ -36,7 +37,7 @@ const ResetPasswordUser = () => {
                      <Col xl={24} lg={24} md={24} sm={24} xs={24}>
                         <Form.Item
                            name="r_password"
-                           label="Xác nhận mật khẩu:"
+                           label="Xác nhận mật khẩu mới:"
                            rules={[{ required: true, message: 'vui lòng xác nhận mật khẩu' }]}
                            style={{ marginLeft: "35%" }}
                         >
